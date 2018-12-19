197990399edf478005fea5df0b63ca5e8e23e4c9
j0h4nn3s <st153749@stud.uni-stuttgart.de>
2018-12-19 10:59:58 +0100

    Reordered modifier

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator extends SuperCalculator {
 
     private String resultAsText;
 
-    public final static int MULTIPLIER = 10;
+    public static final int MULTIPLIER = 10;
 
     public double addition(double a, double b) {
         return a + b;
@@ -16,7 +16,9 @@ public class Calculator extends SuperCalculator {
         return a * b;
     }
 
-    public double multiplication(double a, double b) { return multiplication(a, b, 42); }
+    public double multiplication(double a, double b) {
+        return multiplication(a, b, 42);
+    }
 
     public double subtraction(double a, double b) {
         return a - b;
@@ -29,38 +31,33 @@ public class Calculator extends SuperCalculator {
     public void main() {
         // get numbers
         System.out.println("Enter first and second number:");
-        Scanner inp= new Scanner(System.in);
+        Scanner inp = new Scanner(System.in);
         int num1;
         int num2;
         num1 = inp.nextInt();
         num2 = inp.nextInt();
-
         // get operation
         System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
         int choose;
         choose = inp.nextInt();
-
         // calculate and print result
-        switch (choose){
+        switch(choose) {
             case 1:
-                System.out.println(addition( num1,num2));
+                System.out.println(addition(num1, num2));
                 break;
             case 2:
-                System.out.println(subtraction( num1,num2));
+                System.out.println(subtraction(num1, num2));
                 break;
             case 3:
-                System.out.println(multiplication( num1,num2));
+                System.out.println(multiplication(num1, num2));
                 break;
             case 4:
-                System.out.println(division( num1,num2));
+                System.out.println(division(num1, num2));
                 break;
             default:
                 System.out.println("Illegal Operation");
         }
-
         inp.close();
     }
 }
-
-
 
